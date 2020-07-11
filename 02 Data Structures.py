65bfeaefb65994a77b89f2977d1a0dafebc5b465
vitorrj <52195896+vitorrj@users.noreply.github.com>
2020-07-11 16:38:44 +0200

    New methods such as count, index and worked with strings

--- 02 Data Structures.py
+++ 02 Data Structures.py
@@ -1,4 +1,22 @@
 ### LISTS
-
+mylist = [24, "apple", 1984.0, "vitor"]
+print(mylist[-1])           # "vitor" the last element of the list
+print(len(mylist))          # 2 elements
 
 ### TUPLES
+mytupple = (1,3,4)
+
+print(mytupple)
+##### Slicing and concatenation
+print(mylist[1:3])          # "apple" + 1984.0
+
+##### Repeating a list
+repeatedlist = [0] * 4      # [0, 0, 0, 0]
+print(repeatedlist)
+
+### Counting ocurrences with COUNT
+text = "My name is Vitor. Rodrigues"
+print(text.count("i"))
+myarray = [1,1,3,2,1,1,1,46,4,6]
+print(myarray.count(1))
+
