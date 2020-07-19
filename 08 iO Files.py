4f5b36bde527b90e7457748d5519d8aeec764dcc
vitorj <vitor.rjacinto@gmail.com>
2020-07-19 18:50:57 +0200

    Started Python Functions, Files, and Dictionaries. I/O files

---
+++ 08 iO Files.py
@@ -0,0 +1,30 @@
+# READING FILES
+
+path = "assets/myfile.txt"          # for absolute path we use "../"
+print("------ HEADING ------")
+
+### getting everything on the file
+myfile = open(path, 'r')          # holds a reference to the object which is the file
+text = myfile.read()
+print(text)
+myfile.close()
+
+### getting each line
+myfile = open(path, 'r')
+textlines = myfile.readlines()
+print(textlines)                            # returns a list where each line is an element
+print(len(textlines))                       # return the number of lines
+myfile.close()
+
+### printing all lines
+myfile = open(path, 'r')
+for line in myfile:
+    print(line)
+
+# WRITING FILES
+print("------ WRITING ------")
+
+### writing
+# myfile = open(path, "w")                          # if the file does not exist, it will create one
+# myfile.write(myfile + "Ciao, mondo!")
+# myfile.close()
